b5868da22764af4e6812bfa6ae3395798fa1c2cf
luca <lucafalasca08@gmail.com>
2024-04-23 15:44:14 +0200

    #43 - Cambiare lo stato e le azioni dell'agente in base alle nuove specifiche #44 - Cambiare le bean di comunicazione tra agente e simulatore in base ai nuovi stati e azioni

--- agent/agent.py
+++ agent/agent.py
@@ -15,7 +15,7 @@ from decisions import DecisionTreeConsultant
 from decisions import Experience
 from typing import Callable, Union
 
-from beans import ActionBean, RewardBean, RewardsBean, StateBean
+from beans import ActionBean, RewardBean, StateBean
 
 from agent_factory import AgentEnum, AgentFactory
 from conf_parser import ConfParser
@@ -31,28 +31,18 @@ class AgentFacade():
             self.message_id = message_id
 
     def __init__(self, max_neighbours = 10, agent_description = {"agent_type": AgentEnum.RANDOM_AGENT}):
-
+        self._last_experience = None
         self._max_neighbours = max_neighbours
         node_spec = [
             # one list element for each tensor (state variable) 
-
-            tensor_spec.TensorSpec(shape=(1), dtype=tf.float32, name = "id"),
-            tensor_spec.TensorSpec(shape=(1), dtype=tf.int32, name = "energy_state"),
-            tensor_spec.TensorSpec(shape=(1), dtype=tf.bool, name = "power_state"),
-            tensor_spec.TensorSpec(shape=(1), dtype=tf.bool, name = "has_packet_in_buffer"),
-            
-        ]
-        neighbour_spec = [
-            tensor_spec.TensorSpec(shape=(max_neighbours), dtype=tf.float32, name = "id_neighbour"),
-            tensor_spec.TensorSpec(shape=(max_neighbours), dtype=tf.int32, name = "energy_state_neighbour"),
-            tensor_spec.TensorSpec(shape=(max_neighbours), dtype=tf.float32, name = "link_capacity")
+            tensor_spec.TensorSpec(shape=(1), dtype=tf.float32, name = "energy_level"),
+            tensor_spec.TensorSpec(shape=(1), dtype=tf.float32, name = "queue_state"),
+            tensor_spec.TensorSpec(shape=(1), dtype=tf.float32, name = "charge_rate")
         ]
 
-        observation_spec = node_spec + neighbour_spec
+        observation_spec = node_spec
 
-        action_spec_root = tensor_spec.BoundedTensorSpec(shape=(1), dtype=tf.int32, minimum=0, maximum=1, name = "root_action")
-        action_spec_change_power_state = tensor_spec.BoundedTensorSpec(shape=(1), dtype=tf.int32, minimum=0, maximum=1, name = "change_power_state")
-        action_spec_send_message = tensor_spec.BoundedTensorSpec(shape=(max_neighbours), dtype=tf.float32 , minimum=0, maximum=1, name = "rates")
+        action_spec_root = tensor_spec.BoundedTensorSpec(shape=(), dtype=tf.int32, minimum=0, maximum=2, name = "choose_action")
 
         time_step_spec = ts.time_step_spec(observation_spec)
 
@@ -62,6 +52,7 @@ class AgentFacade():
             time_step_spec = time_step_spec,
             action_spec = action_spec_root)
         
+        '''
         agent_change_power_state = AgentFactory.create_agent(
             agent_description = agent_description, 
             time_step_spec = time_step_spec,
@@ -71,25 +62,24 @@ class AgentFacade():
             agent_description = agent_description, 
             time_step_spec = time_step_spec,
             action_spec = action_spec_send_message)
+        '''
         
         self._root = DecisionTreeConsultant(agent_root, "root")
-        chang_power_state_node = DecisionTreeConsultant(agent_change_power_state, "change_power_state")
-        send_message_node =DecisionTreeConsultant(agent_send_message, "send_message")
-        self._root.add_choice(chang_power_state_node)
-        self._root.add_choice(send_message_node)
-
-        self._unrw_buff = []
+        #chang_power_state_node = DecisionTreeConsultant(agent_change_power_state, "change_power_state")
+        #send_message_node =DecisionTreeConsultant(agent_send_message, "send_message")
+        #self._root.add_choice(chang_power_state_node)
+        #self._root.add_choice(send_message_node)
     
 
     def get_action(self, state_bean, rewards_bean):
         return self._get_action(state_bean, rewards_bean)
     
-    def _get_action(self, state, rewards):
+    def _get_action(self, state, reward):
 
-        # updates agent policy using new rewards
-        experiences = self._assemble_experience_list(rewards)
-        #self._update_policy(experiences)
-        self._root.train(experiences)
+        # updates agent policy using reward from previous action
+        if(reward is not None):
+            exp = Experience(self._last_experience[0], self._last_experience[1], reward)
+            self._root.train([exp])
 
         # Computes TimeStep object by resetting the environment
         # at the given state
@@ -97,59 +87,39 @@ class AgentFacade():
         time_step = state.to_tensor(self._max_neighbours)
         action = []
         self._root.get_decisions(parent_state=time_step, decision_path=action)
-
-        # Uses the action-state pair among the experiences waiting to be rewarded
-        self._unrw_buff.append(self.UnrewardedExperience(state, action, 1))
         
-        action_bean = ActionBean()
-        action_bean.changePowerState = action
+        # Stores the last experience
+        self._last_experience = (state, action)
+
+        action_bean = self._decision_path_to_action_bean(action)
 
         return action_bean
-
-    def _state_to_time_step(self, state):
-        return ts.restart(
-            observation=state.to_tensor(self._max_neighbours)
-        )
-
-    def _assemble_experience_list(self, rewards):
-        experiences = []
-        for r in rewards:
-            for ue in self._unrw_buff:
-                if r.message_id == ue.message_id:
-                    experience = Experience(
-                        state=ue.state.to_tensor(self._max_neighbours),
-                        decision_path=ue.action,
-                        message_id=ue.message_id,
-                        reward=r.reward)
-                    experiences.append(experience)
-        return experiences
-
-    def _assemble_experience(self, state, action, reward):
-        return trajectory.Trajectory(
-            step_type=ts.StepType.MID,
-            observation=state,
-            action=action,
-            policy_info=(),
-            next_step_type=ts.StepType.MID,
-            reward=reward,
-            discount=1.0)
-
-    def _update_policy(self, experiences):
-        for e in experiences:
-            self._agent.train(e)
-            print("Esperienza ricevuta:\n\t Stato [energy:{0}] \n\t Azione [send_message:{1}] \n\t Reward: {2} ".format(e.observation.energy, e.action.action[0][0], e.reward))
+    
+    def _decision_path_to_action_bean(self, decision_path):
+        action = int(decision_path[0].value.action)
+        match action:
+            case 0:
+                return ActionBean(send_message=ActionBean.SendEnum.DO_NOTHING)
+            case 1:
+                return ActionBean(
+                    send_message=ActionBean.SendEnum.SEND_MESSAGE, 
+                    power_source=ActionBean.PowerSourceEnum.BATTERY)
+            case 2:
+                return ActionBean(
+                    send_message=ActionBean.SendEnum.SEND_MESSAGE, 
+                    power_source=ActionBean.PowerSourceEnum.POWER_CHORD)
+            
+                  
             
 # Test main
 if __name__ == '__main__':
     agent_desc = ConfParser.parse_agent_from_json(json.load(open("agent/agent_conf.json")))
     agent = AgentFacade(
         agent_description=agent_desc)
-    neighbour = StateBean(energy=1)
-    state = StateBean(energy=1)
-    state.add_neighbour(neighbour)
-    rewards = [RewardBean(1, 10)]
-    action = agent.get_action(state, [])
-    print("Manda messagio: " + str(action.send_message))
-    action = agent.get_action(state, rewards)
-    print("Manda messagio: " + str(action.send_message))
+    state = StateBean(energy_level=1, queue_state=[1], charge_rate=0)
+    action = agent.get_action(state, None)
+    print("Azione scelta: " + str(action))
+    reward = RewardBean(10)
+    action = agent.get_action(state, reward)
+    print("Azione scelta: " + str(action))
     
