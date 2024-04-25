22e6318e6ecdc60750d491a70219078c3d54c164
Matteo Conti <matteo.conti.977@gmail.com>
2024-04-25 21:56:49 +0200

    close #51

--- simulations/src/srcnode/src_controller.cc
+++ simulations/src/srcnode/src_controller.cc
@@ -61,5 +61,6 @@ void SrcController::sendData()
     data->setData(randomDataGenerator(200.0));
     message_count++;
     send(data, "network_port$o", neigh);
-    scheduleAt(simTime()+exponential(12), new cMessage());
+    simtime_t delay = par("send_interval").doubleValue();
+    scheduleAt(simTime()+delay, new cMessage());
 }
